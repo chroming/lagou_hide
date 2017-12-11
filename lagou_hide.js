804950733b928338c144a5db5112ba6b2ad5c67c
chroming <guangzhishang@gmail.com>
2017-12-11 15:18:27 +0800

    add local storage

--- lagou_hide.js
+++ lagou_hide.js
@@ -4,28 +4,38 @@
 // @version      0.1
 // @description  隐藏不想看到的职位。
 // @author       chroming
-// @match        https://www.lagou.com/*
-// @grant        none
+// @match        https://www.lagou.com/jobs/*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_listValues
 // @require      http://code.jquery.com/jquery-1.11.0.min.js
 // ==/UserScript==
-var position_list = ['3917781', '2561733'];
-var company_list = ['83552', '154789'];
 
+var position_list = JSON.parse(GM_getValue('lagou_hide_pl')||'[]');
+var company_list = JSON.parse(GM_getValue('lagou_hide_cl')||'[]');
 
-append_list = function(id, id_list){
-    console.log(id_list);
-    id_list.push(id);
+append_list = function(id, tag){
+    if (tag === true){
+        position_list.push(id.toString());
+        GM_setValue('lagou_hide_pl', JSON.stringify(position_list));
+    }
+    else {
+        company_list.push(id.toString());
+        GM_setValue('lagou_hide_cl', JSON.stringify(company_list));
+    }
 };
 
 $(function(){
     $('.con_list_item').each(function(){
         var p = $(this).attr('data-positionid');
         var c = $(this).attr('data-companyid');
-        $(this).find('.p_top').append('<button type="button" onclick="append_list(p, position_list)">隐藏职位</button>');
-        $(this).find('.company_name').append('<button type="button" onclick="append_list(c, company_list)">隐藏公司</button>');
         if (position_list.includes(p)||company_list.includes(c)){
             $(this).hide();
             console.log('hide:'+c);
+        }
+        else{
+        $(this).find('.p_top').append('<button type="button" onclick="append_list('+p+', true)">隐藏职位</button>');
+        $(this).find('.company_name').append('<button type="button" onclick="append_list('+c+', false)">隐藏公司</button>');
         }
     });
 });
