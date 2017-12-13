12c0a013805578b739e8a96eeef0b9b48bde0f5d
chroming <github_io@outlook.com>
2017-12-13 17:35:20 +0800

    optimize code

--- lagou_hide.js
+++ lagou_hide.js
@@ -17,7 +17,7 @@ var position_list = JSON.parse(GM_getValue('lagou_hide_pl')||'[]');
 var company_list = JSON.parse(GM_getValue('lagou_hide_cl')||'[]');
 
 append_list = function(id, tag){
-    if (tag === true){
+    if (tag){
         position_list.push(id.toString());
         GM_setValue('lagou_hide_pl', JSON.stringify(position_list));
         GM_notification(text='职位已隐藏', timeout='1');
@@ -26,29 +26,29 @@ append_list = function(id, tag){
     else {
         company_list.push(id.toString());
         GM_setValue('lagou_hide_cl', JSON.stringify(company_list));
-        GM_notification(text='公司已隐藏', timeout='1');
+        GM_notification('公司已隐藏', '1');
     }
     window.location.reload();
 };
 
 var main = function(){
     $('.con_list_item').each(function(){
-        var p = $(this).attr('data-positionid');
-        var c = $(this).attr('data-companyid');
+        var item = $(this);
+        var p = item.attr('data-positionid');
+        var c = item.attr('data-companyid');
         if (position_list.includes(p)||company_list.includes(c)){
-            $(this).hide();
+            item.hide();
             console.log('hide:'+c);
         }
         else{
-            if ($(this).find('.p_top .hide_button').length === 0) {
-                $(this).find('.p_top').append('<button class="hide_button" type="button" onclick="append_list('+p+', true)">隐藏职位</button>');
-                $(this).find('.company_name').append('<button type="button" onclick="append_list('+c+', false)">隐藏公司</button>');
+            if (item.find('.p_top .hide_button').length === 0) {
+                item.find('.p_top').append('<button class="hide_button" type="button" onclick="append_list('+p+', true)">隐藏职位</button>');
+                item.find('.company_name').append('<button type="button" onclick="append_list('+c+', false)">隐藏公司</button>');
         }}
     });
 };
 $('.pager_container [hidefocus]').each($(this).click(
     function(){
-        //$(document).ajaxComplete(main);
         setTimeout(main, 1000);
     }
 
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 在拉勾隐藏你不想看到的职位。
 
-tampermonkey/Greasemonkey 脚本。
+tampermonkey 脚本。
 
 *开发中，欢迎提出建议*
